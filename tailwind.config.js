d655089ab6a133d460d0cbf90e7e2b18e6c19d08
Stephen Alonzo <stephena0228@gmail.com>
2023-03-20 18:14:50 -0700

    installed tailwind to webpack and node dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./resources/views/*.blade.php", 
-    "./chirper-html/*.html"
+    "./resources/views/*.blade.php",
+    "./resources/views/**/*.blade.php",
   ],
   theme: {
     extend: {},
     screens: {
-      "lg": '992px'
-    }
+      "xs": '480px',
+      'sm': '576px',
+      'md': '768px',
+      "lg": '992px',
+      "xl": '1200px'
+  }
   },
   plugins: [],
 }
